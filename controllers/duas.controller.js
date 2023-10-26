b37b31facf77fd14f113da2c5859b368f65337a8
ReactAppDeveloper <waaneirfan@gmail.com>
2023-10-26 17:43:23 +0500

    Dua Api More Including

--- controllers/duas.controller.js
+++ controllers/duas.controller.js
@@ -16,10 +16,29 @@ const CreateDuas= asyncHandler(async (req, res) => {
   });
   res.status(200).json(dua);
 });
-
+const UpdateDuas = asyncHandler(async (req, res) => {
+  const dua = {_id: req.params.id};
+  Duas.updateOne(dua,req.body)
+  .then(duaid=>{
+    if(!duaid){return res.status(404).end();}
+    return res.status(200).json(duaid)
+  })
+});
+const DeleteDuas = asyncHandler(async (req, res) => {
+ Duas.findByIdAndRemove(req.params.id)
+ .exec()
+ .then(dua=>{
+  if(!dua){return res.status(404).end();}
+  return  res.status(200).json({
+    message: "Dua Deleted Successfully",
+  });
+ })
+});
 
 
 module.exports = {
   getDuas,
   CreateDuas,
+  UpdateDuas,
+  DeleteDuas
 };
