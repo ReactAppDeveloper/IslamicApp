6ef83238c4f57bdb4865dd442a521354bbbf3684
ReactAppDeveloper <waaneirfan@gmail.com>
2023-11-02 17:53:05 +0500

    Duas Api Updated 01 Nov

--- controllers/duas.controller.js
+++ controllers/duas.controller.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const Duas = require("../models/duas");
+const mongoose = require("mongoose");
 //@desc Get all Hadees books
 //@route GET /api/hadeesbooks
 //@access private
@@ -7,6 +8,12 @@ const getDuas= asyncHandler(async (req, res) => {
   const duas = await Duas.find()
     .sort({ id: 1 });
   res.status(200).json(duas);
+});
+
+const getindDuas = asyncHandler(async (req, res) => {
+    const {_id}  = req.body;
+  const dua = await Duas.find({ _id })
+  res.status(200).json(dua);
 });
 
 const CreateDuas= asyncHandler(async (req, res) => {
@@ -38,6 +45,7 @@ const DeleteDuas = asyncHandler(async (req, res) => {
 
 module.exports = {
   getDuas,
+  getindDuas,
   CreateDuas,
   UpdateDuas,
   DeleteDuas
--- routes/dua.routes.js
+++ routes/dua.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { getDuas, CreateDuas, UpdateDuas, DeleteDuas } = require("../controllers/duas.controller");
+const { getDuas,getindDuas, CreateDuas, UpdateDuas, DeleteDuas } = require("../controllers/duas.controller");
 
 router.route("/").get(getDuas).post(CreateDuas);
+router.route("/").post(getindDuas);
 router.route("/:id").put(UpdateDuas);
 router.route("/:id").delete(DeleteDuas);
 module.exports = router;
