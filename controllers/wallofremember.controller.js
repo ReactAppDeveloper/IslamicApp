7e9b7ae58053e37ddf4573ef25488c1d020d199a
Waiiz <mac@Mac.lan>
2026-09-10 14:52:12 +0500

    New Api of Wall of Remember

--- controllers/wallofremember.controller.js
+++ controllers/wallofremember.controller.js
@@ -6,8 +6,15 @@ const getWallOfRemember = asyncHandler(async (req, res) => {
     const wallofremembers = await WallOfRemember.find()
      .sort({ _id: 1 });
     res.status(200).json(wallofremembers);
-  });
+});
+
+const getWallOfRememberByID = asyncHandler(async (req, res) => {
+  const wallofremembers = await WallOfRemember.find(new mongoose.Types.ObjectId(req.params.id))
+  res.status(200).json(wallofremembers);
+});
+
   
 module.exports = {
     getWallOfRemember,
+    getWallOfRememberByID
 };
